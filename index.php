b4e2dae43c539428f6bad52f5ed2e583c7f2ca1b
Salami <salamisodikiolawale@gmail.com>
2021-07-10 13:00:58 +0200

    Firts commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/style.css">
+    <script defer src="public/js/index.js"></script>
+    <title>Todo</title>
+</head>
+<body>
+    <header>HEADER</header>
+    <div class="content">CONTENT</div>
+    <footer>FOOTER</footer>
+    
+</body>
+</html>
