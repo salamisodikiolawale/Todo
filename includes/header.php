8c5fad1ca2862f3700f5c305a5e7b918b1da0bcc
Salami <salamisodikiolawale@gmail.com>
2021-07-12 01:07:38 +0200

    First commit

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<header>
+    <a href="/" class="logo">Blog</a>
+    <ul class="header-menu">
+        <li>
+            <a href="/add-article.php">Ecrire un article</a>
+        </li>
+    </ul>
+</header>
